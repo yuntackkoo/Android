02b73443b3862175354a97c054758922f021680b
eunjaeshin <ejshin0214@gmail.com>
2017-06-07 10:42:46 +0900

    스크롤뷰

--- SSD/app/src/main/java/ssd/app/Devlist.java
+++ SSD/app/src/main/java/ssd/app/Devlist.java
@@ -43,7 +43,7 @@ public class Devlist extends AppCompatActivity {
         lv_Devlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                
+
             }
         });
 
